db9385c773535a7284356d835e4db6a0a281d610
Logan Best <logan@loganbest.net>
2024-06-27 18:02:36 -0400

    cleaning up a few old things

--- module/version.tf
+++ module/version.tf
@@ -1,11 +1,10 @@
 terraform {
-  required_version = ">= 1.5"
+  required_version = ">= 1.8"
 
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = "~> 5.8"
-      configuration_aliases = [aws.net_prod, aws.net_prod_use1, aws.net_prod_use2]
+      version               = "~> 5.50"
     }
     external = {
       source  = "hashicorp/external"
