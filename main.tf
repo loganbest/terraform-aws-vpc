ffcedcba22076749e5f8efdbb2bb7fa9f286170d
Logan Best <logan@loganbest.net>
2024-10-10 12:17:14 -0400

    stamping as a working version for ipv4 at least

--- main.tf
+++ main.tf
@@ -1,66 +1,68 @@
 data "aws_region" "current" {}
 
-data "aws_vpc_ipam_pool" "this" {
-  count = var.enable_ipam ? 1 : 0
+data "aws_caller_identity" "current" {}
 
-  filter {
-    name   = "description"
-    values = ["*${var.ipam_pool}*"]
-  }
-
-  filter {
-    name   = "address-family"
-    values = ["ipv4"]
-  }
-}
-
-# Preview next CIDR from pool
-data "aws_vpc_ipam_preview_next_cidr" "this" {
-  count = var.enable_ipam ? 1 : 0
-
-  ipam_pool_id   = data.aws_vpc_ipam_pool.this[0].id
-  netmask_length = var.cidr_mask_length
-}
+data "aws_partition" "current" {}
 
 data "external" "subnet_calculator" {
-  count = var.enable_ipam ? 1 : 0
-
   program = ["python3", "${path.module}/subnet.py"]
 
   query = {
-    base_cidr = data.aws_vpc_ipam_preview_next_cidr.this[0].cidr
-    num_azs   = length(var.region_config.az_ids)
+    base_cidr          = var.vpc_cidr
+    num_azs            = length(var.region_config.az_ids)
+    enable_fw_subnets  = var.enable_anf
+    enable_tgw_subnets = (var.tgw_id == null) ? true : false
   }
 }
 
 locals {
-  private_subnets  = (!var.enable_ipam && length(var.private_subnets_cidrs) > 0) ? var.private_subnets_cidrs : try(split(",", data.external.subnet_calculator[0].result["private_subnets"]), [])
-  firewall_subnets = (!var.enable_ipam && length(var.firewall_subnets_cidrs) > 0) ? var.firewall_subnets_cidrs : try(split(",", data.external.subnet_calculator[0].result["firewall_subnets"]), [])
-  public_subnets   = (!var.enable_ipam && length(var.public_subnets_cidrs) > 0) ? var.public_subnets_cidrs : try(split(",", data.external.subnet_calculator[0].result["public_subnets"]), [])
+  private_subnets  = (!var.enable_ipam && length(var.private_subnets_cidrs) > 0) ? var.private_subnets_cidrs : try(split(",", data.external.subnet_calculator.result["private_subnets"]), [])
+  firewall_subnets = (!var.enable_ipam && length(var.firewall_subnets_cidrs) > 0) ? var.firewall_subnets_cidrs : try(split(",", data.external.subnet_calculator.result["firewall_subnets"]), [])
+  tgw_subnets      = (!var.enable_ipam && length(var.tgw_subnets_cidrs) > 0) ? var.tgw_subnets_cidrs : try(split(",", data.external.subnet_calculator.result["tgw_subnets"]), [])
+  public_subnets   = (!var.enable_ipam && length(var.public_subnets_cidrs) > 0) ? var.public_subnets_cidrs : try(split(",", data.external.subnet_calculator.result["public_subnets"]), [])
 }
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.13.0"
+################################################################################
+# VPC
+################################################################################
 
-  # vpc
-  cidr                          = (var.enable_ipam) ? data.aws_vpc_ipam_preview_next_cidr.this[0].cidr : var.vpc_cidr
-  name                          = var.name
-  instance_tenancy              = "default"
-  enable_dns_hostnames          = true
-  manage_default_security_group = true
-  manage_default_route_table    = true
-  manage_default_network_acl    = true
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
 
-  default_security_group_ingress = var.default_security_group_ingress
-  default_security_group_egress  = var.default_security_group_egress
+  ipv6_cidr_block   = var.enable_ipv6 ? var.ipv6_cidr : null
+  ipv6_ipam_pool_id = var.enable_ipv6 ? var.ipv6_ipam_pool_id : null
+
+  instance_tenancy                     = "default"
+  enable_dns_hostnames                 = var.enable_dns_hostnames
+  enable_dns_support                   = var.enable_dns_support
+  enable_network_address_usage_metrics = var.enable_network_address_usage_metrics
+
+  tags = merge(
+    module.this.tags,
+    var.vpc_tags,
+  )
 }
+
+#module "vpc" {
+#source  = "terraform-aws-modules/vpc/aws"
+#version = "~> 5.13.0"
+
+
+#enable_flow_log             = var.enable_flow_log
+#flow_log_destination_type   = var.flow_log_destination_type
+#flow_log_traffic_type       = var.flow_log_traffic_type
+#flow_log_destination_arn    = var.flow_log_destination_arn
+#flow_log_file_format        = var.flow_log_file_format
+#flow_log_per_hour_partition = var.flow_log_per_hour_partition
+
+#tags = module.this.tags
+#}
 
 # VPC Peering with defined VPCs
 resource "aws_vpc_peering_connection" "this" {
   for_each = { for vpc in var.vpc_peers : vpc.vpc_id => vpc }
 
-  vpc_id        = module.vpc.vpc_id
+  vpc_id        = aws_vpc.this.id
   peer_vpc_id   = each.value.vpc_id
   peer_owner_id = each.value.account_id
   peer_region   = each.value.region
